2f43df3db5e5b5498ffc8c91afc039f53ea93722
ALFAMODO <alfamodo.official@gmail.com>
2024-02-19 22:01:41 -0500

    portfolio

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import Image from 'next/image';
 import Link from 'next/link';
-import AboutImg from '../../public/assets/AboutMe.png';
+import AboutImg from '../../public/assets/About.png';
 
 const About = () => {
   return (
